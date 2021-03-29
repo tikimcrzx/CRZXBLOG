2dfbeda433e377790fe648eaf916c6d454fcbca0
tikimcrzx <tikimioo723@gmail.com>
2021-03-28 23:24:23 -0700

    Initial Structure Project

---
+++ Persistence/Migrations/20210329051359_RenamePropertie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class RenamePropertie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Avenue",
+                table: "Activities",
+                newName: "Venue");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Venue",
+                table: "Activities",
+                newName: "Avenue");
+        }
+    }
+}
