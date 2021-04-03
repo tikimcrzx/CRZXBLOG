0d1c0cebf7fdda6a71adae1bc77d548e406cf156
tikimcrzx <tikimioo723@gmail.com>
2021-04-03 01:38:13 -0700

    Initial Structure React and added Axios

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,23 @@
+import { Grid } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+import ActivityList from './ActivityList';
+
+interface Props {
+  activities: IActivity[];
+}
+
+export default function ActivityDashboard({ activities }: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {activities[0] && <ActivityDetails activity={activities[0]} />}
+        <ActivityForm />
+      </Grid.Column>
+    </Grid>
+  );
+}
