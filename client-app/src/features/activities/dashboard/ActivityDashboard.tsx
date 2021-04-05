5321f4264b0bf18e5a0414d7190432f0106356a5
tikimcrzx <tikimioo723@gmail.com>
2021-04-04 21:29:44 -0600

    Add react-router and initial structure

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,10 +4,19 @@ import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
   const { selectedActivity, editMode } = activityStore;
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading App" />;
 
   return (
     <Grid>
